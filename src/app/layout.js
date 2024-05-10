9cca6b87f517d47cb189f3b315b7f48538378afd
Engerberth Reyes <engerberthr73@gmail.com>
2024-05-10 01:41:37 -0400

    wewe

---
+++ src/app/layout.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Iniciar Sesión",
+  description: "Hecho en Next.Js",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Image from "next/image";
+import stylesLogin from "./CSS/styles-login.module.css";
+
+const Index = () => {
+  const mostrarPassword = () => {
+    alert("Le Diste Click a La Contraseña");
+  }
+  console.log("Nuevo Mensaje");
+  return (
+    <>
+      <section className={`${stylesLogin.contenedor_general} w-50`}></section>
+      <form className={stylesLogin.contenedor_form}>
+        <h1 className={stylesLogin.titulo_form}>Iniciar Sesión</h1>
+        <label className={stylesLogin.label}>Algao</label>
+        <input className={`${stylesLogin.input_texto} rounded-2`} type="text" />
+        <label className={stylesLogin.label}>Algo 2</label>
+        <section className={stylesLogin.contenedor_input_password}>
+        <input className={`${stylesLogin.input_texto} rounded-2`} type="text" />
+        <Image className={stylesLogin.icono_password} onClick={mostrarPassword} width={20} height={20} src={`/eye-solid.svg`} />
+        </section>
+        <button className={`${stylesLogin.boton} rounded-2`}>Boton Aca</button>
+      </form>
+    </>
+  );
+};
+
+export default Index;
