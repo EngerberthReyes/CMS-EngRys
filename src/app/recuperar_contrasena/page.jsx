cef15cf921cc28cf582815f47313ce46402c62ef
Engerberth Reyes <engerberthr73@gmail.com>
2024-05-10 22:59:27 -0400

    asd

--- src/app/recuperar_contrasena/page.jsx
+++ src/app/recuperar_contrasena/page.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import stylesLogin from "../CSS/styles-login.module.css";
+import stylesLogin from "../CSS/styles-recuperarContrasena.module.css";
 
 const RecuperarContraseña = () => {
   const mostrarPassword = () => {
@@ -16,37 +16,32 @@ const RecuperarContraseña = () => {
         <link rel="icon" href="/eye-solid.svg" />
       </head>
       <body>
-        <section className={`${stylesLogin.contenedor_general} w-50`}></section>
-        <form className={stylesLogin.contenedor_form}>
-          <h1 className={stylesLogin.titulo_form}>Registrar Algo</h1>
-          <label className={stylesLogin.label}>Algo</label>
+      <section className={stylesLogin.contenedor_general}></section>
+      <form className={stylesLogin.contenedor_form}>
+        <h1 className={stylesLogin.titulo_form}>Registrar Algo</h1>
+        <label className={stylesLogin.label}>Algo</label>
+        <input className={`${stylesLogin.input_texto} rounded-2`} type="text" />
+        <label className={stylesLogin.label}>Algo 2</label>
+        <section className={stylesLogin.contenedor_input_password}>
           <input
             className={`${stylesLogin.input_texto} rounded-2`}
             type="text"
           />
-          <label className={stylesLogin.label}>Algo 2</label>
-          <section className={stylesLogin.contenedor_input_password}>
-            <input
-              className={`${stylesLogin.input_texto} rounded-2`}
-              type="text"
-            />
-            <Image
-              className={stylesLogin.icono_password}
-              onClick={mostrarPassword}
-              width={20}
-              height={20}
-              src={`/eye-solid.svg`}
-            />
-            <section className={stylesLogin.cotenedor_passoword_perdida}>
-              <Link className={stylesLogin.link_password} href="/">
-                ¿Ya dispone de una cuenta?
-              </Link>
-            </section>
+          <Image
+            className={stylesLogin.icono_password}
+            onClick={mostrarPassword}
+            width={20}
+            height={20}
+            src={`/eye-solid.svg`}
+          />
+          <section className={stylesLogin.cotenedor_passoword_perdida}>
+            <Link className={stylesLogin.link_password} href="/">
+              ¿Ya dispone de una cuenta?
+            </Link>
           </section>
-          <button className={`${stylesLogin.boton} rounded-2`}>
-            Registrarse
-          </button>
-        </form>
+        </section>
+        <button className={`${stylesLogin.boton} rounded-2`}>Registrarse</button>
+      </form>
       </body>
     </>
   );
