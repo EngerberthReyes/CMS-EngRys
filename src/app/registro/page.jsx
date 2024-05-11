a72b1d342c508573690a3d01459749d66c454b9d
Engerberth Reyes <engerberthr73@gmail.com>
2024-05-10 23:26:25 -0400

    as

--- src/app/registro/page.jsx
+++ src/app/registro/page.jsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import stylesLogin from "../CSS/styles-login.module.css";
+import stylesRegistro from "../CSS/styles-registro.module.css";
 
 const Registro = () => {
   const mostrarPassword = () => {
@@ -16,34 +16,34 @@ const Registro = () => {
         <link rel="icon" href="/eye-solid.svg" />
       </head>
       <body>
-        <section className={`${stylesLogin.contenedor_general} w-50`}></section>
-        <form className={stylesLogin.contenedor_form}>
-          <h1 className={stylesLogin.titulo_form}>Registrar Algo</h1>
-          <label className={stylesLogin.label}>Algo</label>
+        <section className={`${stylesRegistro.contenedor_general} w-50`}></section>
+        <form className={stylesRegistro.contenedor_form}>
+          <h1 className={stylesRegistro.titulo_form}>Registrar Algo</h1>
+          <label className={stylesRegistro.label}>Algo</label>
           <input
-            className={`${stylesLogin.input_texto} rounded-2`}
+            className={`${stylesRegistro.input_texto} rounded-2`}
             type="text"
           />
-          <label className={stylesLogin.label}>Algo 2</label>
-          <section className={stylesLogin.contenedor_input_password}>
+          <label className={stylesRegistro.label}>Algo 2</label>
+          <section className={stylesRegistro.contenedor_input_password}>
             <input
-              className={`${stylesLogin.input_texto} rounded-2`}
+              className={`${stylesRegistro.input_texto} rounded-2`}
               type="text"
             />
             <Image
-              className={stylesLogin.icono_password}
+              className={stylesRegistro.icono_password}
               onClick={mostrarPassword}
               width={20}
               height={20}
               src={`/eye-solid.svg`}
             />
-            <section className={stylesLogin.cotenedor_passoword_perdida}>
-              <Link className={stylesLogin.link_password} href="/">
+            <section className={stylesRegistro.cotenedor_passoword_perdida}>
+              <Link className={stylesRegistro.link_password} href="/">
                 ¿Ya dispone de una cuenta?
               </Link>
             </section>
           </section>
-          <button className={`${stylesLogin.boton} rounded-2`}>
+          <button className={`${stylesRegistro.boton} rounded-2`}>
             Registrarse
           </button>
         </form>
