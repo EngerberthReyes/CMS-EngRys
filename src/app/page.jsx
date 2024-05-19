52d20cd16dcd7f7b1275c864649043bfe33e0486
Engerberth Reyes <engerberthr73@gmail.com>
2024-05-19 00:33:01 -0400

    GRID

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,38 +20,38 @@ const Inicio = () => {
           </button>
         </header>
         <main className={stylesInicio.main}>
-        <section className={stylesInicio.seccionPrincipal}>
-          <section>
-        <h1>Aqui va algo</h1>
-          <section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-            <section>Cajas</section>
-          </section>
-          </section>
-        </section>
-        <section className={stylesInicio.seccionSecundaria}>
-        <section>
-            <h1>Lateral</h1>
+          <section className={stylesInicio.seccionPrincipal}>
             <section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
-            <section>Cajas2</section>
+              <h1>Aqui va algo</h1>
+              <section className={stylesInicio.seccionElementos}>
+                <section>Cajas</section>
+                <section>Cajas</section>
+                <section>Cajas</section>
+                <section>Cajas</section>
+                <section>Cajas</section>
+                <section>Cajas</section>
+                <section>Cajas</section>
+                <section>Cajas</section>
+                <section>Cajas</section>
+              </section>
+            </section>
           </section>
+          <section className={stylesInicio.seccionSecundaria}>
+            <section>
+              <h1>Lateral</h1>
+              <section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+                <section>Cajas2</section>
+              </section>
+            </section>
           </section>
-        </section>
         </main>
       </body>
     </>
