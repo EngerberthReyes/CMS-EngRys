4a7826100e68cd3876dccb92bb4bee5234788a05
Engerberth Reyes <engerberthr73@gmail.com>
2024-05-18 22:01:44 -0400

    asd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,13 +12,43 @@ const Inicio = () => {
         <title>inicio - CMS</title>
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body style={{ alignItems: "flex-start" }}>
+      <body style={{ justifyContent: "flex-start", alignItems: "flex-start", flexDirection: "column", rowGap: "5%"}}>
         <header className={stylesInicio.header}>
-          <h1>Header</h1>
+          <h1>CMS</h1>
           <button className={`${stylesInicio.boton} rounded-2`}>
             Iniciar Sesión
           </button>
         </header>
+        <section className={stylesInicio.seccionPrincipal}>
+          <section>
+        <h1>Aqui va algo</h1>
+          <section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+            <section>Cajas</section>
+          </section>
+          </section>
+          <section>
+            <h1>Lateral</h1>
+            <section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+            <section>Cajas2</section>
+          </section>
+          </section>
+        </section>
       </body>
     </>
   );
