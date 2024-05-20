7ccd65e1780ab75093f5b3a931fdc7d4b5e8a9b0
Engerberth Reyes <engerberthr73@gmail.com>
2024-05-20 17:59:15 -0400

    Actualizando Registro

--- src/app/registro/page.jsx
+++ src/app/registro/page.jsx
@@ -16,15 +16,37 @@ const Registro = () => {
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body id={stylesRegistro.body}>
-        <section className={`${stylesRegistro.contenedor_general} w-50`}></section>
+        <section
+          className={`${stylesRegistro.contenedor_general} w-50`}
+        ></section>
         <form className={stylesRegistro.contenedor_form}>
           <h1 className={stylesRegistro.titulo_form}>Registrar Cuenta</h1>
-          <label className={stylesRegistro.label}>Algo</label>
+          <label className={stylesRegistro.label}>Nombres</label>
           <input
             className={`${stylesRegistro.input_texto} rounded-2`}
             type="text"
           />
-          <label className={stylesRegistro.label}>Algo 2</label>
+          <label className={stylesRegistro.label}>Apellidos</label>
+          <input
+            className={`${stylesRegistro.input_texto} rounded-2`}
+            type="text"
+          />
+          <label className={stylesRegistro.label}>Cédula</label>
+          <input
+            className={`${stylesRegistro.input_texto} rounded-2`}
+            type="text"
+          />
+          <label className={stylesRegistro.label}>Nacionalidad</label>
+          <input
+            className={`${stylesRegistro.input_texto} rounded-2`}
+            type="text"
+          />
+          <label className={stylesRegistro.label}>Correo Electrónico</label>
+          <input
+            className={`${stylesRegistro.input_texto} rounded-2`}
+            type="text"
+          />
+          <label className={stylesRegistro.labelClave}>Contraseña</label>
           <section className={stylesRegistro.contenedor_input_password}>
             <input
               className={`${stylesRegistro.input_texto} rounded-2`}
@@ -38,6 +60,25 @@ const Registro = () => {
               src={`/eye-solid.svg`}
             />
           </section>
+          <label className={stylesRegistro.labelClave}>Repetir Contraseña</label>
+          <section className={stylesRegistro.contenedor_input_password}>
+            <input
+              className={`${stylesRegistro.input_texto} rounded-2`}
+              type="text"
+            />
+            <Image
+              className={stylesRegistro.icono_password}
+              onClick={mostrarPassword}
+              width={20}
+              height={20}
+              src={`/eye-solid.svg`}
+            />
+          </section>
+          <label className={stylesRegistro.label}>Fecha de Nacimiento</label>
+          <input
+            className={`${stylesRegistro.input_texto} rounded-2`}
+            type="text"
+          />
           <section className={stylesRegistro.contenedor_passoword_perdida}>
             <Link
               className={stylesRegistro.link_password}
