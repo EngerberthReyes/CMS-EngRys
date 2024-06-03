088e49c6d0e29558153eef611fcfdd3d661b6944
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-03 00:02:06 -0400

    ERRORASO

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -117,163 +117,174 @@ const Perfil = () => {
 
   return (
     <>
-      <head>
-        <title>Perfil - EpíComputers</title>
-        <link rel="icon" href="./IMG/IconoNoLineal.png" />
-      </head>
-      <body id={stylesPerfil.body}>
-        <header className={stylesPerfil.header}>
-          <Link className={stylesPerfil.enlaceNormal} href={"/"}>
-            <h1 className={stylesPerfil.tituloHeader}>EpíComputers</h1>
-          </Link>
-          <section
-            className={`${stylesPerfil.seccionEnlace} ${stylesPerfil.seccionEnlaceAumentada}`}
-          >
-            <Link className={stylesPerfil.enlace} href="/noticias">
-              Noticias
-            </Link>
-            <Link className={stylesPerfil.enlace} href="/sobre_nosotros">
-              Sobre Nosotros
-            </Link>
-            <Link className={stylesPerfil.enlace} href="/contactanos">
-              Contactanos
-            </Link>
-          </section>
-          <section className={stylesPerfil.seccionEnlace}>
-            {usuario ? (
-              <>
-                <Link
-                  className={`${stylesPerfil.enlace} ${stylesPerfil.usuarioPerfil}`}
-                  href="/perfil"
-                >
-                  <section>{usuario.nombreDeUsuario}</section>
-                  <section>{usuario.correoElectronicoDeUsuario}</section>
+      {usuario === undefined ? (
+        <h1>ERROR</h1>
+      ) : (
+        <>
+          <head>
+            <title>Perfil - EpíComputers</title>
+            <link rel="icon" href="./IMG/IconoNoLineal.png" />
+          </head>
+          <body id={stylesPerfil.body}>
+            <header className={stylesPerfil.header}>
+              <Link className={stylesPerfil.enlaceNormal} href={"/"}>
+                <h1 className={stylesPerfil.tituloHeader}>EpíComputers</h1>
+              </Link>
+              <section
+                className={`${stylesPerfil.seccionEnlace} ${stylesPerfil.seccionEnlaceAumentada}`}
+              >
+                <Link className={stylesPerfil.enlace} href="/noticias">
+                  Noticias
                 </Link>
-                <button
-                  className={`${stylesPerfil.enlace} ${stylesPerfil.usuarioPerfil}`}
-                  onClick={() => cerrarPerfil()}
-                >
-                  <section>Cerrar Sesión</section>
-                </button>
-              </>
-            ) : (
-              <>
-                <Link className={stylesPerfil.enlace} href="/iniciar_sesion">
-                  Iniciar Sesión
+                <Link className={stylesPerfil.enlace} href="/sobre_nosotros">
+                  Sobre Nosotros
                 </Link>
-                <Link className={stylesPerfil.enlace} href="/registro">
-                  Registrarse
+                <Link className={stylesPerfil.enlace} href="/contactanos">
+                  Contactanos
                 </Link>
-              </>
-            )}
-          </section>
-        </header>
-        <main>
-          <section className={stylesPerfil.main}>
-            <section className={stylesPerfil.seccionSecundaria}>
-              <section className={stylesPerfil.seccionAjustes}>
-                <h1 className={stylesPerfil.titulo}>Perfil</h1>
-                <section className={stylesPerfil.seccionFlex}>
-                  <section className={stylesPerfil.seccionImagen}>
-                    <Image
-                      className={stylesPerfil.imagenes}
-                      width={200}
-                      height={200}
-                      src={imagen ? imagen : "/IMG/epigrafe73.png"}
-                      alt={
-                        nombreImagen
-                          ? nombreImagen
-                          : "Imagen de Perfil Por Defecto"
-                      }
-                    />
-                    {temaActual === "oscuro" ? (
-                      <>
-                        <label
-                          className={stylesPerfil.label}
-                          htmlFor="cambiarImagen"
-                          title="Cambiar Foto de Perfil"
-                        >
-                          <section className={stylesPerfil.seccionOscura}>
-                            <Image
-                              className={stylesPerfil.icono_edit}
-                              onClick={() => cambiarImagen}
-                              width={20}
-                              height={20}
-                              src={`/editar-theme-white.svg`}
-                              alt="Cambiar Foto de Perfil"
-                            />
-                            <input
-                              id="cambiarImagen"
-                              onChange={agregarImagen}
-                              accept=".gif, .png, ,.apng, .jpg, .jpeg"
-                              type="file"
-                            />
-                          </section>
-                        </label>
-                      </>
-                    ) : (
-                      <>
-                        <label
-                          className={stylesPerfil.label}
-                          htmlFor="cambiarImagen"
-                          title="Cambiar Foto de Perfil"
-                        >
-                          <section className={stylesPerfil.seccionBlanco}>
-                            <Image
-                              className={stylesPerfil.icono_edit}
-                              onClick={() => cambiarImagen}
-                              width={20}
-                              height={20}
-                              src={`/editar-theme-black.svg`}
-                              alt="Cambiar Foto de Perfil"
-                            />
-                            <input
-                              id="cambiarImagen"
-                              onChange={agregarImagen}
-                              accept=".gif, .png, ,.apng, .jpg, .jpeg"
-                              type="file"
-                            />
-                          </section>
-                        </label>
-                      </>
-                    )}
-                  </section>
-                  <section className={stylesPerfil.seccionPerfilIzquierdo}>
-                    <h1>Tu Nombre:</h1>
-                    <section>
-                      {usuario && <h1>{usuario.nombreDeUsuario}</h1>}
+              </section>
+              <section className={stylesPerfil.seccionEnlace}>
+                {usuario ? (
+                  <>
+                    <Link
+                      className={`${stylesPerfil.enlace} ${stylesPerfil.usuarioPerfil}`}
+                      href="/perfil"
+                    >
+                      <section>{usuario.nombreDeUsuario}</section>
+                      <section>{usuario.correoElectronicoDeUsuario}</section>
+                    </Link>
+                    <button
+                      className={`${stylesPerfil.enlace} ${stylesPerfil.usuarioPerfil}`}
+                      onClick={() => cerrarPerfil()}
+                    >
+                      <section>Cerrar Sesión</section>
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <Link
+                      className={stylesPerfil.enlace}
+                      href="/iniciar_sesion"
+                    >
+                      Iniciar Sesión
+                    </Link>
+                    <Link className={stylesPerfil.enlace} href="/registro">
+                      Registrarse
+                    </Link>
+                  </>
+                )}
+              </section>
+            </header>
+            <main>
+              <section className={stylesPerfil.main}>
+                <section className={stylesPerfil.seccionSecundaria}>
+                  <section className={stylesPerfil.seccionAjustes}>
+                    <h1 className={stylesPerfil.titulo}>Perfil</h1>
+                    <section className={stylesPerfil.seccionFlex}>
+                      <section className={stylesPerfil.seccionImagen}>
+                        <Image
+                          className={stylesPerfil.imagenes}
+                          width={200}
+                          height={200}
+                          src={imagen ? imagen : "/IMG/epigrafe73.png"}
+                          alt={
+                            nombreImagen
+                              ? nombreImagen
+                              : "Imagen de Perfil Por Defecto"
+                          }
+                        />
+                        {temaActual === "oscuro" ? (
+                          <>
+                            <label
+                              className={stylesPerfil.label}
+                              htmlFor="cambiarImagen"
+                              title="Cambiar Foto de Perfil"
+                            >
+                              <section className={stylesPerfil.seccionOscura}>
+                                <Image
+                                  className={stylesPerfil.icono_edit}
+                                  onClick={() => cambiarImagen}
+                                  width={20}
+                                  height={20}
+                                  src={`/editar-theme-white.svg`}
+                                  alt="Cambiar Foto de Perfil"
+                                />
+                                <input
+                                  id="cambiarImagen"
+                                  onChange={agregarImagen}
+                                  accept=".gif, .png, ,.apng, .jpg, .jpeg"
+                                  type="file"
+                                />
+                              </section>
+                            </label>
+                          </>
+                        ) : (
+                          <>
+                            <label
+                              className={stylesPerfil.label}
+                              htmlFor="cambiarImagen"
+                              title="Cambiar Foto de Perfil"
+                            >
+                              <section className={stylesPerfil.seccionBlanco}>
+                                <Image
+                                  className={stylesPerfil.icono_edit}
+                                  onClick={() => cambiarImagen}
+                                  width={20}
+                                  height={20}
+                                  src={`/editar-theme-black.svg`}
+                                  alt="Cambiar Foto de Perfil"
+                                />
+                                <input
+                                  id="cambiarImagen"
+                                  onChange={agregarImagen}
+                                  accept=".gif, .png, ,.apng, .jpg, .jpeg"
+                                  type="file"
+                                />
+                              </section>
+                            </label>
+                          </>
+                        )}
+                      </section>
+                      <section className={stylesPerfil.seccionPerfilIzquierdo}>
+                        <h1>Tu Nombre:</h1>
+                        <section>
+                          {usuario && <h1>{usuario.nombreDeUsuario}</h1>}
+                        </section>
+                      </section>
+                      <section className={stylesPerfil.seccionPerfilIzquierdo}>
+                        <h1>Sobre Mi:</h1>
+                        <section>
+                          <h2>{"Texto Sobre Mi"}</h2>
+                        </section>
+                      </section>
                     </section>
                   </section>
-                  <section className={stylesPerfil.seccionPerfilIzquierdo}>
-                    <h1>Sobre Mi:</h1>
-                    <section>
-                      <h2>{"Texto Sobre Mi"}</h2>
+                </section>
+                <section className={stylesPerfil.seccionTerciaria}>
+                  <section className={stylesPerfil.seccionAjustes}>
+                    <h1>Informacion Personal</h1>
+                    <section className={stylesPerfil.seccionFlex}>
+                      <section>
+                        {usuario && (
+                          <h1>{usuario.correoElectronicoDeUsuario}</h1>
+                        )}
+                      </section>
+                      <section>Cajas2</section>
+                      <section>Cajas2</section>
+                      <section>Cajas2</section>
+                      <section>Cajas2</section>
+                      <section>Cajas2</section>
+                      <section>Cajas2</section>
+                      <section>Cajas2</section>
+                      <section>Cajas2</section>
                     </section>
                   </section>
                 </section>
               </section>
-            </section>
-            <section className={stylesPerfil.seccionTerciaria}>
-              <section className={stylesPerfil.seccionAjustes}>
-                <h1>Informacion Personal</h1>
-                <section className={stylesPerfil.seccionFlex}>
-                  <section>
-                    {usuario && <h1>{usuario.correoElectronicoDeUsuario}</h1>}
-                  </section>
-                  <section>Cajas2</section>
-                  <section>Cajas2</section>
-                  <section>Cajas2</section>
-                  <section>Cajas2</section>
-                  <section>Cajas2</section>
-                  <section>Cajas2</section>
-                  <section>Cajas2</section>
-                  <section>Cajas2</section>
-                </section>
-              </section>
-            </section>
-          </section>
-        </main>
-      </body>
+            </main>
+          </body>
+        </>
+      )}
     </>
   );
 };
