f0f2407f75b078915e52d577f7b0e811d52b1b8f
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-04 23:35:06 -0400

    sdf

--- src/componentes/tiptap/TipTap.jsx
+++ src/componentes/tiptap/TipTap.jsx
@@ -23,30 +23,35 @@ const MenuBar = ({ editor }) => {
     <section className="menuBar">
       <section>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleBold().run()}
           className={editor.isActive("bold") ? "is_active" : ""}
         >
           <FaBold />
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleItalic().run()}
           className={editor.isActive("italic") ? "is_active" : ""}
         >
           <FaItalic />
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleUnderline().run()}
           className={editor.isActive("underline") ? "is_active" : ""}
         >
           <FaUnderline />
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleStrike().run()}
           className={editor.isActive("strike") ? "is_active" : ""}
         >
           <FaStrikethrough />
         </button>
         <button
+          type="button"
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
@@ -57,6 +62,7 @@ const MenuBar = ({ editor }) => {
           <FaHeading />
         </button>
         <button
+          type="button"
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
@@ -67,18 +73,21 @@ const MenuBar = ({ editor }) => {
           <FaHeading className="heading3" />
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleBulletList().run()}
           className={editor.isActive("bulletList") ? "is_active" : ""}
         >
           <FaListUl />
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
           className={editor.isActive("orderedList") ? "is_active" : ""}
         >
           <FaListOl />
         </button>
         <button
+          type="button"
           onClick={() => editor.chain().focus().toggleBlockquote().run()}
           className={editor.isActive("blockquote") ? "is_active" : ""}
         >
@@ -86,10 +95,16 @@ const MenuBar = ({ editor }) => {
         </button>
       </section>
       <section>
-        <button onClick={() => editor.chain().focus().undo().run()}>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().undo().run()}
+        >
           <FaUndo />
         </button>
-        <button onClick={() => editor.chain().focus().redo().run()}>
+        <button
+          type="button"
+          onClick={() => editor.chain().focus().redo().run()}
+        >
           <FaRedo />
         </button>
       </section>
