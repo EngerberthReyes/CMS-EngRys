ad82ab1e0614a629f2166385fb164f8de6a3a9bc
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-09 17:14:54 -0400

    asd

--- src/app/API/cookiesActualizar/route.js
+++ src/app/API/cookiesActualizar/route.js
@@ -6,39 +6,38 @@ import { cmsConexion } from "@/db/database";
 export const GET = async (request) => {
   try {
 
-    // Obtener la cookie de la solicitud
     const cookieValue = request.cookies.get("cookieInformacion").value;
-console.log(cookieValue)
+    console.log(cookieValue);
 
-const a = verify(cookieValue, "secret")
-console.log(a)
-    // Verificar si la cookie existe
+    const a = verify(cookieValue, "secret");
+
     if (!cookieValue) {
-      throw new Error("La cookie 'cookieInformacion' no se encontró en la solicitud.");
+      throw new Error(
+        "La cookie 'cookieInformacion' no se encontró en la solicitud."
+      );
     }
 
-    // Consultar la base de datos para obtener la información actualizada del perfil
     const consultaActualizacionPerfil = `SELECT id_persona, fotoPerfil FROM personas AS p WHERE correo_electronico = ?;`;
-    const actualizacionPerfil = await cmsConexion.query(consultaActualizacionPerfil, [a.correoElectronicoDeUsuario]);
+    const actualizacionPerfil = await cmsConexion.query(
+      consultaActualizacionPerfil,
+      [a.correoElectronicoDeUsuario]
+    );
 
-    // Decodificar la cookie JWT para obtener los datos originales
     const decodedToken = verify(cookieValue, "secret");
 
-    // Modificar los datos según sea necesario
     decodedToken.fotoPerfil = actualizacionPerfil[0].fotoPerfil;
 
-    // Codificar los datos modificados en un nuevo JWT
     const nuevoToken = sign(decodedToken, "secret");
 
-    console.log(nuevoToken)
-    const e = verify(nuevoToken, "secret")
-    console.log(e)
-    // Crear una nueva cookie con el nuevo JWT
+    console.log(nuevoToken);
+    const e = verify(nuevoToken, "secret");
+    console.log(e);
+
     const nuevaCookie = serialize("cookieInformacion", nuevoToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60 * 24 * 30 * 100, // 30 días
-      path: "/", // Ruta de acceso de la cookie
+      maxAge: 60 * 60 * 24 * 30 * 100,
+      path: "/",
     });
 
     // Configurar la nueva cookie en la respuesta
@@ -56,5 +55,3 @@ console.log(a)
     return new NextResponse("Error interno del servidor", { status: 500 });
   }
 };
-
-
