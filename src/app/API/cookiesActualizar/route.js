69921c56949ad934a1b35bf26b9e4a57b7669c0c
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-09 17:14:10 -0400

    sdf

--- src/app/API/cookiesActualizar/route.js
+++ src/app/API/cookiesActualizar/route.js
@@ -37,7 +37,7 @@ console.log(a)
     const nuevaCookie = serialize("cookieInformacion", nuevoToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60 * 24 * 30, // 30 días
+      maxAge: 60 * 60 * 24 * 30 * 100, // 30 días
       path: "/", // Ruta de acceso de la cookie
     });
 
