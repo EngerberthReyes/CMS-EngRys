cef48247672c762a67f2e7101f723d6c0038e231
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-14 02:08:53 -0400

    ad

--- src/app/API/publicaciones/route.js
+++ src/app/API/publicaciones/route.js
@@ -8,8 +8,28 @@ export const POST = async (request) => {
   try {
     const postEnviado = await request.json();
     console.log(postEnviado);
+    const cookieValue = request.cookies.get("cookieInformacion").value;
+    console.log(cookieValue);
 
-    return NextResponse.json(publicaciones);
+    const verificacionCookie = verify(cookieValue, "secret");
+    console.log(verificacionCookie);
+
+    if (!cookieValue) {
+      throw new Error(
+        "La cookie 'cookieInformacion' no se encontró en la solicitud."
+      );
+    }
+
+    const consultaActualizacionPerfil = `SELECT id_persona FROM personas AS p WHERE id_persona = ?;`;
+    const actualizacionPerfil = await cmsConexion.query(
+      consultaActualizacionPerfil,
+      [verificacionCookie.idPersona]
+    );
+
+    const idPersona = actualizacionPerfil[0].id_persona;
+    console.log(idPersona);
+
+    return NextResponse.json(postEnviado);
   } catch (error) {
     console.error("Error al modificar la cookie con JWT:", error);
     return new NextResponse("Error interno del servidor", { status: 500 });
