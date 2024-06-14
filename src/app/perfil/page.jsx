ef8e5b2fcb1320c2e8af6164791188705b1360e9
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-13 21:01:29 -0400

    asd

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -237,7 +237,7 @@ const Perfil = () => {
   useEffect(() => {
     obtenerPerfil();
     console.log("xd");
-  }, [usuario]);
+  }, [interruptorCambio]);
 
   const cerrarPerfil = async () => {
     try {
