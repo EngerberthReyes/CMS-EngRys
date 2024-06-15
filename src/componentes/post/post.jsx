d322b4165b8fc946857c4060ccf5611b5aba5066
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-15 18:19:55 -0400

    xd

--- src/componentes/post/post.jsx
+++ src/componentes/post/post.jsx
@@ -9,203 +9,184 @@ import { useState, useEffect } from "react";
 
 const Post = ({ post, nombreDeUsuario, usuario }) => {
   console.log(post);
-
-  const [nombreDeUsuarioBD, setNombreDeUsuarioBD] = useState("");
-
-  useEffect(() => {
-    if (!post || !Array.isArray(post)) return;
-
-    const nombresDeUsuarios = post
-      .map((elementoUsuario, index) => {
-        const nombreCompleto = `${elementoUsuario.nombre} ${elementoUsuario.apellido}`;
-        const nombreApellido = nombreCompleto ? nombreCompleto.split(" ") : [];
-        const nombreDeUsuario = `${nombreApellido[0]} ${
-          nombreApellido[nombreApellido.length - 1]
-        }`;
-        return nombreDeUsuario;
-      });
-
-    setNombreDeUsuarioBD(nombresDeUsuarios[nombresDeUsuarios.length - 1]);
-  }, [post]);
-
-  console.log(nombreDeUsuarioBD);
-
   return (
     <>
       {post.length > 0
-        ? post
-            .map((elementoPost, index) => (
-              <section key={index} className={stylesPost.seccionPrincipal}>
-                <section className={stylesPost.seccionGrid}>
-                  <section style={{ display: "flex", width: "95%" }}>
-                    {!usuario ? (
-                      <>
-                        <Link
-                          className={`${stylesPost.enlace} ${stylesPost.usuarioPerfil}`}
-                          style={{
-                            border: "none",
-                            wordBreak: "keep-all",
-                            height: "4rem",
-                            margin: "0 0 1rem 0",
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            cursor: "default",
-                          }}
-                          href=""
-                        >
-                          <Image
-                            className={stylesPost.imagenes}
-                            width={35}
-                            height={20}
-                            src={
-                              usuario?.fotoPerfil
-                                ? usuario.fotoPerfil
-                                : "/IMG/epigrafe73.png"
-                            }
-                          />
-                          <section style={{ wordBreak: "keep-all" }}>
-                            <section>{nombreDeUsuario}</section>
-                          </section>
-                        </Link>
-                      </>
-                    ) : (
-                      <>
-                        <Link
-                          className={`${stylesPost.enlace} ${stylesPost.usuarioPerfil}`}
-                          style={{
-                            border: "none",
-                            wordBreak: "keep-all",
-                            height: "4rem",
-                            margin: "0 0 1rem 0",
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            cursor: "default",
-                          }}
-                          href=""
-                        >
-                          <Image
-                            className={stylesPost.imagenes}
-                            width={35}
-                            height={20}
-                            src={
-                              elementoPost.fotoPerfil
-                                ? elementoPost.fotoPerfil
-                                : "/IMG/epigrafe73.png"
-                            }
-                          />
-                          <section style={{ wordBreak: "keep-all" }}>
-                            <section>{nombreDeUsuarioBD}</section>
-                          </section>
-                        </Link>
-                      </>
-                    )}
-                  </section>
-                  <section>
+        ? post.map((elementoPost, index) => (
+            <section key={index} className={stylesPost.seccionPrincipal}>
+              <section className={stylesPost.seccionGrid}>
+                <section style={{ display: "flex", width: "95%" }}>
+                  {!usuario ? (
                     <>
-                      {elementoPost.mensaje && (
-                        <Details description={elementoPost.mensaje} />
-                      )}
+                      <Link
+                        className={`${stylesPost.enlace} ${stylesPost.usuarioPerfil}`}
+                        style={{
+                          border: "none",
+                          wordBreak: "keep-all",
+                          height: "4rem",
+                          margin: "0 0 1rem 0",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          cursor: "default",
+                        }}
+                        href=""
+                      >
+                        <Image
+                          className={stylesPost.imagenes}
+                          width={35}
+                          height={20}
+                          src={
+                            usuario?.fotoPerfil
+                              ? usuario.fotoPerfil
+                              : "/IMG/epigrafe73.png"
+                          }
+                        />
+                        <section style={{ wordBreak: "keep-all" }}>
+                          <section>{nombreDeUsuario}</section>
+                        </section>
+                      </Link>
                     </>
-                    {elementoPost.enlaces && (
-                      <>
-                        {elementoPost.enlaces.map((enlace, index) => (
-                          <>
-                            <Link
-                              key={index}
-                              className={stylesPost.links}
-                              href={`${enlace} `}
-                              target="_blank"
-                            >
-                              {enlace}
-                            </Link>
-                            <br />
-                          </>
-                        ))}
-                      </>
-                    )}
-                  </section>
-                  {elementoPost.imagen.length > 0 && (
-                    <section className={stylesPost.lineaPunteada}></section>
+                  ) : (
+                    <>
+                      <Link
+                        className={`${stylesPost.enlace} ${stylesPost.usuarioPerfil}`}
+                        style={{
+                          border: "none",
+                          wordBreak: "keep-all",
+                          height: "4rem",
+                          margin: "0 0 1rem 0",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                          cursor: "default",
+                        }}
+                        href=""
+                      >
+                        <Image
+                          className={stylesPost.imagenes}
+                          width={35}
+                          height={20}
+                          src={
+                            elementoPost.fotoPerfil
+                              ? elementoPost.fotoPerfil
+                              : "/IMG/epigrafe73.png"
+                          }
+                        />
+                        <section style={{ wordBreak: "keep-all" }}>
+                          <section>
+                            {`${elementoPost.nombre.split(" ")[0]}
+                              ${elementoPost.apellido.split(" ")[0]}`}
+                          </section>
+                        </section>
+                      </Link>
+                    </>
                   )}
-                  <section className={stylesPost.seccionGridImagenes}>
-                    {elementoPost.imagenUrl && (
-                      <>
-                        {elementoPost.imagenUrl.map((url, index) => (
-                          <img
-                            className={stylesPost.imagen}
-                            key={index}
-                            src={url}
-                            alt="Imagen de Internet (URL)"
-                          />
-                        ))}
-                      </>
+                </section>
+                <section>
+                  <>
+                    {elementoPost.mensaje && (
+                      <Details description={elementoPost.mensaje} />
                     )}
-                    {elementoPost.imagen.length > 0 && (
-                      <>
-                        {elementoPost.imagen.map((archivo, index) => (
-                          <>
-                            <section key={index}>
-                              {archivo.includes(".mp4") ? (
-                                <>
-                                  <video
-                                    className={stylesPost.imagen}
-                                    src={archivo}
-                                    property
-                                    fill
-                                    controls
-                                  ></video>
-                                </>
-                              ) : (
-                                <img
+                  </>
+                  {elementoPost.enlaces && (
+                    <>
+                      {elementoPost.enlaces.map((enlace, index) => (
+                        <>
+                          <Link
+                            key={index}
+                            className={stylesPost.links}
+                            href={`${enlace} `}
+                            target="_blank"
+                          >
+                            {enlace}
+                          </Link>
+                          <br />
+                        </>
+                      ))}
+                    </>
+                  )}
+                </section>
+                {elementoPost.imagen.length > 0 && (
+                  <section className={stylesPost.lineaPunteada}></section>
+                )}
+                <section className={stylesPost.seccionGridImagenes}>
+                  {elementoPost.imagenUrl && (
+                    <>
+                      {elementoPost.imagenUrl.map((url, index) => (
+                        <img
+                          className={stylesPost.imagen}
+                          key={index}
+                          src={url}
+                          alt="Imagen de Internet (URL)"
+                        />
+                      ))}
+                    </>
+                  )}
+                  {elementoPost.imagen.length > 0 && (
+                    <>
+                      {elementoPost.imagen.map((archivo, index) => (
+                        <>
+                          <section key={index}>
+                            {archivo.includes(".mp4") ? (
+                              <>
+                                <video
                                   className={stylesPost.imagen}
                                   src={archivo}
-                                />
-                              )}
-                            </section>
-                          </>
-                        ))}
-                        {elementoPost.youtubeUrl.length > 0 && (
-                          <section>
-                            {elementoPost.imagen.length === 0 && (
-                              <section
-                                className={stylesPost.lineaPunteada}
-                              ></section>
-                            )}
-                            {elementoPost.youtubeUrl.map(
-                              (videoYoutube, index) => (
-                                <>
-                                  <iframe
-                                    key={elementoPost.id_publicacion}
-                                    style={{ width: "100%" }}
-                                    src={videoYoutube.replace(
-                                      "watch?v=",
-                                      "embed/"
-                                    )}
-                                    title={`youtube-video-${index}`}
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                    allowFullScreen
-                                    className={stylesPost.imagen}
-                                  ></iframe>
-                                </>
-                              )
+                                  property
+                                  fill
+                                  controls
+                                ></video>
+                              </>
+                            ) : (
+                              <img
+                                className={stylesPost.imagen}
+                                src={archivo}
+                              />
                             )}
                           </section>
-                        )}
-                      </>
-                    )}
-                  </section>
-                  <section className={stylesPost.lineaPunteada}></section>
-                  <section className={stylesPost.seccionElementos}>
-                    <section className={stylesPost.item}>Cajas</section>
-                    <section className={stylesPost.item}>Cajas</section>
-                    <section className={stylesPost.item}>Cajas</section>
-                    <section className={stylesPost.item}>Cajas</section>
-                  </section>
+                        </>
+                      ))}
+                      {elementoPost.youtubeUrl.length > 0 && (
+                        <section>
+                          {elementoPost.imagen.length === 0 && (
+                            <section
+                              className={stylesPost.lineaPunteada}
+                            ></section>
+                          )}
+                          {elementoPost.youtubeUrl.map(
+                            (videoYoutube, index) => (
+                              <>
+                                <iframe
+                                  key={elementoPost.id_publicacion}
+                                  style={{ width: "100%" }}
+                                  src={videoYoutube.replace(
+                                    "watch?v=",
+                                    "embed/"
+                                  )}
+                                  title={`youtube-video-${index}`}
+                                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                  allowFullScreen
+                                  className={stylesPost.imagen}
+                                ></iframe>
+                              </>
+                            )
+                          )}
+                        </section>
+                      )}
+                    </>
+                  )}
+                </section>
+                <section className={stylesPost.lineaPunteada}></section>
+                <section className={stylesPost.seccionElementos}>
+                  <section className={stylesPost.item}>Cajas</section>
+                  <section className={stylesPost.item}>Cajas</section>
+                  <section className={stylesPost.item}>Cajas</section>
+                  <section className={stylesPost.item}>Cajas</section>
                 </section>
               </section>
-            ))
+            </section>
+          ))
         : null}
     </>
   );
