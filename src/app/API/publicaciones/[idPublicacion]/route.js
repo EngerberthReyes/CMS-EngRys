137e1e9b8f8d701e384df5632140a1e240c3aee9
Engerberth Reyes <engerberthr73@gmail.com>
2024-06-15 21:39:01 -0400

    cf

--- src/app/API/publicaciones/[idPublicacion]/route.js
+++ src/app/API/publicaciones/[idPublicacion]/route.js
@@ -10,12 +10,12 @@ export const DELETE = async (request, { params: { idPublicacion } }) => {
   try {
     console.log(idPublicacion);
 
-    const consultaPerfil = `SELECT id_persona FROM publicaciones WHERE id_publicacion = ?;`;
-    const recoleccionId = await cmsConexion.query(consultaPerfil, [
+    const consultaPublicacion = `DELETE FROM publicaciones WHERE id_publicacion = ?;`;
+    const respuestaPublicacion = await cmsConexion.query(consultaPublicacion, [
       Number(idPublicacion),
     ]);
 
-    return NextResponse.json(recoleccionId);
+    return NextResponse.json(respuestaPublicacion);
   } catch (error) {
     console.error(error);
     return new NextResponse("Error interno del servidor", { status: 500 });
